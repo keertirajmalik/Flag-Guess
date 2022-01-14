09709b7b26339ed71620cc4b8da939f8153fd56a
keertirajmalik <keertirajmalik@gmail.com>
2022-01-14 19:12:51 +0530

    Initial Commit

---
+++ Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 2
+//
+//  Created by Keertiraj Laxman Malik on 14/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
